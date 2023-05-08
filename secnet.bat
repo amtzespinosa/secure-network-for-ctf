d24862ceab29a2a32e286a860a5958a67daf72c1
Alex Martínez <76045216+amtzespinosa@users.noreply.github.com>
2023-05-08 23:39:02 +0200

    Add files via upload

---
+++ secnet.bat
@@ -0,0 +1,2 @@
+cd C:/Program Files/Oracle/VirtualBox/
+vboxmanage dhcpserver add --network=SecureNetwork --server-ip=192.168.1.1 --lower-ip=192.168.1.10 --upper-ip=192.168.1.200 --netmask=255.255.255.0 --enable
